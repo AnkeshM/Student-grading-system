5eb56bce7eabc209ca49f93a3f23466c604203cd
AnkeshM <ankeshmishra997@gmail.com>
2025-04-06 10:14:31 +0530

    modified:   my-next-app/app/api/updatesem/route.js

--- my-next-app/app/api/updatesem/route.js
+++ my-next-app/app/api/updatesem/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
 import User from "/models/user";
-import Semester from "/models/semesters"; // Assuming you have this model
+import Semester from "/models/semesters";
 
 export async function POST(req) {
   try {
